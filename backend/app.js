cbc1dd42bf41a6505583aa2fc40173b3f413528d
maintain0404 <maintain0404@gmail.com>
2021-03-22 23:02:20 +0900

    realtime chatting

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,6 @@
 const express = require('express');
-const socket = require('socket.io');
 const app = express();
 const server = app.listen(5000);
-
-app.use(cors());
 
 const io = require('socket.io')(server, {
     cors: {
@@ -21,6 +18,21 @@ io.on('connection', (socket) => {
     // io.emit() 모든 클라에 응답
     // HTTP처럼 똑같이 헤더 날라감
 
-    socket.emit('hello','안녕하세요');
-    io.emit('hello everyone', '모두 안녕하세요')  
-})
+    // socket.emit('hello','안녕하세요');
+    // io.emit('hello everyone', '모두 안녕하세요')  
+
+    // 서버에선 socket.on ~
+    console.log('Hello world!');
+    io.emit('sendText', {
+        text: '대충 서버에서 넘어온 값'
+    });
+
+    socket.on("sendMessage", (data) => {
+        //string
+
+        // 데이터 주는 역할
+        io.emit("sendMessage",{
+            text: data
+        });
+    });
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
 function App() {
+  const [Text, setText] = useState([{text: "안녕하세요"}]);
+  const [Value, setValue] = useState('');
   const socket = io.connect('http://localhost:5000');
-  useEffect(() => {
-    socket.on('hello', (data) => {
-      console.log(data);
-    })
-  }, [])
+  // useEffect(() => {
+  //   socket.on('hello', (data) => {
+  //     console.log(data);
+  //   })
+  // }, [])
+
+  // useEffect(() => {
+  //   socket.on('hello everyone', (data) => {
+  //     console.log(data);
+  //   })
+  // }, [])
+
+  function handleClick(e) {
+    console.log(Value);
+    socket.emit("send Message", Value);
+    setValue("");
+  }
+  
+  function handleChange(e) {
+    e.stopPropagation();
+    setValue(e.target.value);
+    console.log(Value);
+  }
 
   useEffect(() => {
-    socket.on('hello everyone', (data) => {
+    // setText([...Text, { text: "으아아아"}]) // 이런 방식으로 동적으로 설정 가능함
+    socket.on("send Message", (data) => {
+      setText((Text) => Text.concat(data));
       console.log(data);
-    })
-  }, [])
+    });
+  }, []);
 
   return (
-    <div className="App">
-      안녕하세요?
+    <div>
+      {Text.map((data) => {
+        return <div>{data.text}</div>
+      })}
+      <input type="text" value={Value} onChange={handleChange} />
+      <button onClick={handleClick}>전송</button>
     </div>
   );
 }
